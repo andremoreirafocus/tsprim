22bd6a11e0fa309ba5ba506813c1ecea57b9afec
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-06-03 18:24:14 -0300

    feat: multer initial tests

---
+++ src/multer/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
+
+app.use("/", express.static("."));
+
+app.post("/doupload", upload.single("uploaded_file"), function (
+  req,
+  res,
+  next
+) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  const { file } = req;
+  console.log(file);
+  return res.json(file);
+});
+
+app.listen(3333);
