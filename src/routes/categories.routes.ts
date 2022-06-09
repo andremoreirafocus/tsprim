f6fb6e9411eb2d5e4353d6e99cc7f8baef1167ce
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-06-09 19:51:10 -0300

    feat: importCategoriesUseCase complete

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
+
+const upload = multer({ dest: "uploads/" });
 
 const categoriesRouter = Router();
 
 import createCategoryController from "../useCases/createCategory/CreateCategoryController";
 import listCategoriesController from "../useCases/listCategories/ListCategoriesController";
+import importCategoriesController from "../useCases/importCategories/ImportCategoriesController";
 
 categoriesRouter.post("/", (request, response) => {
   createCategoryController.handle(request, response);
@@ -12,5 +16,13 @@ categoriesRouter.post("/", (request, response) => {
 categoriesRouter.get("/", (request, response) => {
   listCategoriesController.handle(request, response);
 });
+
+categoriesRouter.post(
+  "/import",
+  upload.single("uploaded_file"),
+  (request, response) => {
+    importCategoriesController.handle(request, response);
+  }
+);
 
 export { categoriesRouter };
--- src/useCases/importCategories/ImportCategoriesHandler.ts
+++ src/useCases/importCategories/ImportCategoriesHandler.ts
@@ -1,16 +1,40 @@
 import { Request, Response } from "express";
+import Category from "../../modules/cars/models/Category";
 import { IImportCategoriesUseCase } from "./IImportCategoriesUseCase";
+import { readFile } from "fs/promises";
 
 export default class ImportCategoryHandler {
   constructor(private importCategoriesUseCase: IImportCategoriesUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    const { categories } = request.body;
+  async handle(request: Request, response: Response): Promise<Response> {
     try {
+      const { file } = request;
+      console.log(file);
+      // return response.json(file);
+      const categoriesListFileName = file.path;
+      const categories = await this.extractCategoriesFromFile(
+        categoriesListFileName
+      );
       this.importCategoriesUseCase.execute(categories);
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
     return response.status(201).send();
+  }
+
+  async extractCategoriesFromFile(file: any): Promise<Category[]> {
+    const lines = await this.getFileContent(file);
+    const categories = [];
+    for (const line of lines.split("\r\n")) {
+      console.log(line);
+      const [name, description] = line.split(";");
+      categories.push({ name, description });
+    }
+    return categories;
+  }
+
+  async getFileContent(fileName: any) {
+    const fileContent = await readFile(fileName, "utf8");
+    return fileContent;
   }
 }
