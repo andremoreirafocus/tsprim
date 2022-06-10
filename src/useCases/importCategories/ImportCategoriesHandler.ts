f32063545432cd868e1e2d2d5e21de7be6b3b3bd
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-06-10 16:15:08 -0300

    refactor: importCategoriesController using streams

--- src/useCases/importCategories/ImportCategoriesHandler.ts
+++ src/useCases/importCategories/ImportCategoriesHandler.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import Category from "../../modules/cars/models/Category";
 import { IImportCategoriesUseCase } from "./IImportCategoriesUseCase";
 import { readFile } from "fs/promises";
+import fs from "fs";
+import { parse as csvParser } from "csv-parse";
 
 export default class ImportCategoryHandler {
   constructor(private importCategoriesUseCase: IImportCategoriesUseCase) {}
@@ -11,17 +13,37 @@ export default class ImportCategoryHandler {
       const { file } = request;
       console.log(file);
       const categoriesListFileName = file.path;
-      const categories = await this.extractCategoriesFromFile(
-        categoriesListFileName
-      );
-      this.importCategoriesUseCase.execute(categories);
+      const stream = fs.createReadStream(categoriesListFileName);
+      const csvFileParser = csvParser({
+        delimiter: ";",
+      });
+      const lines = [];
+      stream.pipe(csvFileParser);
+      csvFileParser.on("data", async (line) => {
+        // console.log(line);
+        lines.push(line);
+      });
+      csvFileParser.on("end", () => {
+        const categories = this.extractCategoriesFromLines(lines);
+        this.importCategoriesUseCase.execute(categories);
+        return response.status(201).send();
+      });
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
-    return response.status(201).send();
   }
 
-  async extractCategoriesFromFile(file: string): Promise<Category[]> {
+  extractCategoriesFromLines(lines): Category[] {
+    const categories = [];
+    for (const line of lines) {
+      console.log(line);
+      const [name, description] = line;
+      categories.push({ name, description });
+    }
+    return categories;
+  }
+
+  async extractCategoriesFromFileOld(file: string): Promise<Category[]> {
     const lines = await this.getFileContent(file);
     const categories = [];
     for (const line of lines.split("\r\n")) {
