02d839ed99cc2741654750ee9567271993c0f967
Andre Moreira <andre.moreira@focus-sys.com.br>
2022-06-16 13:50:03 -0300

    refactor: rename models to entities

--- src/modules/cars/repositories/CategoriesMemoryRepository.ts
+++ src/modules/cars/repositories/CategoriesMemoryRepository.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from "uuid";
 
-import Category from "../models/Category";
+import Category from "../entities/Category";
 import { ICategoriesRepository } from "./ICategoriesRepository";
 
 interface ICreateCategoryDTO {
